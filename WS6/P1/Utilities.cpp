d77819fe69662b8cb484342f60afb37b77026d83
Mehtab Singh Jagde <mehtabsj003@gmail.com>
2024-03-07 14:40:35 -0500

    added workshop 6 part 1

---
+++ WS6/P1/Utilities.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Employee.h"
+#include "Utilities.h"
+#include <string>
+#include <sstream>
+namespace seneca {
+	Person* buildInstance(std::istream& in) {
+		std::string line{};
+		std::stringstream tempVar{};
+		Person* personPtr{};
+		getline(in, line, '\n');
+		tempVar << line;
+		if (line[0] == 'e' || line[0] == 'E')
+		{
+			personPtr = new Employee(tempVar);
+		}
+		/*
+		else if (line[0] == 's' || line[0] == 'S') {
+			personPtr = new Student(tempStream);
+		}
+		else if (line[0] == 'p' || line[0] == 'P') {
+			personPtr = new Professor(tempStream);
+		}
+		 */
+		else
+		{
+			personPtr = nullptr;
+		}
+
+		return personPtr;
+	}
+	bool isDigit(std::string& src) {
+		auto begin = src.begin();
+		while (begin != src.end() && std::isdigit(*begin)) {
+			++begin;
+		}
+		return !src.empty() && begin == src.end();
+	}
+	std::string& trim(std::string& src) {
+		bool valid = false;
+
+		while (!valid)
+		{
+			if (src[0] == ' ')
+			{
+				src.erase(0, src.find_first_not_of(' '));
+			}
+			else if (src[src.length() - 1] == ' ')
+			{
+				src.erase(src.find_last_not_of(' ') + 1);
+			}
+			else
+			{
+				valid = true;
+			}
+		}
+		return src;
+	}
+}
